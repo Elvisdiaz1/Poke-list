f3c83ad2cabd334f74410b2f994dea21264baf69
Elvis Diaz <elvisdiaz020@gmail.com>
2023-06-09 18:09:03 -0400

    got api working

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let api = "https://pokeapi.co/api/v2/pokemon";
+const api = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
 
 let showPokeData = async () => {
   try {
     const res = await fetch(api);
     const data = await res.json();
-    showData(data);
+    console.log(data);
+    // showData(data);
   } catch (error) {
     console.log(error);
   }
 };
 
-function showData(apiData) {}
+function showData(apiData) {
+  const pokeList = document.createElement("div");
+  let firstSet = apiData.results.filter((pokemon, index) => {
+    return index < 20;
+  });
+  pokeList.appendChild(firstSet);
+}
+
+showPokeData();
