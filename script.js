4016c78dfc91023076169db10f175f8e28159c1d
Elvis Diaz <elvisdiaz020@gmail.com>
2023-06-13 18:26:37 -0400

    makes api work to show pokemon (first 20)

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-const api = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-
+const box = new Set();
 let showPokeData = async () => {
-  try {
-    const res = await fetch(api);
-    const data = await res.json();
-    console.log(data);
-    // showData(data);
-  } catch (error) {
-    console.log(error);
+  for (let i = 0; i < 21; i++) {
+    const api = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    try {
+      const res = await fetch(api);
+      const data = await res.json();
+      console.log(data);
+      //   showData(box);
+      box.add(data);
+      placeData();
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
-function showData(apiData) {
+// function showData(apiData) {
+//   //   let pokeSet = apiData.results.filter((pokemon, index) => {
+//   //     return index < 21;
+//   //   });
+
+//   box.map((box) => {
+//     const pokeList = document.createElement("div");
+//     const pokeSprite = document.createElement("img");
+//     pokeSprite.src = box.sprites.front_default;
+//     pokeList.appendChild(pokeSprite);
+//     document.body.appendChild(pokeList);
+//   });
+// }
+
+function placeData() {
+  // Need to find a way to only place one copy of each pokemon without
+  // having to do document.body.innerHTML for efficieny
+  // because the console still places dupes of the pokemon
+  document.body.innerHTML = "";
   const pokeList = document.createElement("div");
-  let firstSet = apiData.results.filter((pokemon, index) => {
-    return index < 20;
+  box.forEach((pokemon) => {
+    const pokeSprite = document.createElement("img");
+    pokeSprite.src = pokemon.sprites.front_default;
+    pokeList.appendChild(pokeSprite);
+    document.body.appendChild(pokeList);
   });
-  pokeList.appendChild(firstSet);
 }
 
 showPokeData();
+console.log(box);
